57e55d8b5a84e820c2873a6ea862686a6bdc8803
elizabeth-reber <elizabeth.szathmary@gmail.com>
2021-04-09 15:02:12 -0700

    commit

---
+++ Pythagorean/Pythagorean.java
@@ -0,0 +1,7 @@
+import java.lang;
+
+public class Pythagorean {
+    public static double calculateHypotenuse (int lenA, int lenB) {
+        return Math.sqrt((lenA * lenA ) + (lenB * lenB));
+    }
+}
