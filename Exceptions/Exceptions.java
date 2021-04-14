678004e2277b0066b56fed3c1f474a2c7b2630fa
elizabeth-reber <elizabeth.szathmary@gmail.com>
2021-04-14 14:45:49 -0700

    Exceptions

---
+++ Exceptions/Exceptions.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Set;
+
+public class ArrayList {
+    public static void main (String[] args) {
+        ArrayList<Object> myList = (new ArrayList<Objerct>());
+        myList.add("13");
+        myList.add("Helo world");
+        myList.add(48);
+        myList.add("Goodbye World");
+
+        for(int i = 0; i < myList.size(); i++) {
+            Integer castedValue = (integer myList.get(i));
+
+        System.out.println(list);
+        }
+        }
+    }
+}
