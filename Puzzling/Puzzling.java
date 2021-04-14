4a5f4118ae5d3e3f7d2a2a71a9cb4f275094b0c1
elizabeth-reber <elizabeth.szathmary@gmail.com>
2021-04-14 08:33:18 -0700

    Hashmaps

--- Puzzling/Puzzling.java
+++ Puzzling/Puzzling.java
@@ -3,18 +3,30 @@ import java.util.Collections;
 import java.util.Random;
 
 public class Puzzling{
-    public static void main(String[] args){
-        Random rand = new Random()
-        ArrayList<Character> alphabet = new ArrayList<Character>()
-        for(char ch = 'a'; ch <= 'z'; ++ch) {
+    public static void main(String[] args) {
+        ArrayList<Character> alphabet = new ArrayList<Character>();
+        for(char ch = 'a'; ch <= 'z'; ++ch){
             alphabet.add(ch);
         }
-        System.out.println(alphabet)
-        collections.shuffle(alphabet)
-        System.out.println(alphabet.get(25));
-        int randNum = rand.nextInt(25);
-        System.out.println(randNum);
-        System.out.println(alphabet.get(randNum));
+
+        System.out.println(alphabet);
 
     }
 }
+
+// public class Puzzling{
+//     public static void main(String[] args){
+//         Random rand = new Random()
+//         ArrayList<Character> alphabet = new ArrayList<Character>()
+//         for(char ch = 'a'; ch <= 'z'; ++ch) {
+//             alphabet.add(ch);
+//         }
+//         System.out.println(alphabet)
+//         collections.shuffle(alphabet)
+//         System.out.println(alphabet.get(25));
+//         int randNum = rand.nextInt(25);
+//         System.out.println(randNum);
+//         System.out.println(alphabet.get(randNum));
+
+//     }
+// }
